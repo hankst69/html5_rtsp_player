aaa8a76bd95e96dab305a23d0d40eb929f68fbcc
hankst69 <steffen_hanke@web.de>
2024-02-15 23:07:16 +0100

    some experimental changes to example code

---
+++ clone.bat
@@ -0,0 +1,3 @@
+@rem https://github.com/hankst69/html5_rtsp_player
+@rem https://github.com/Streamedian/html5_rtsp_player
+@call "%~dp0..\clone_in_folder.bat" "%0" "https://github.com/hankst69/html5_rtsp_player.git"
